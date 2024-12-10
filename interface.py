998b760f254e0e1f7de1c355f7c41b42cb614d85
Ssbgoku82 <159487842+Ssbgoku82@users.noreply.github.com>
2024-12-09 22:19:46 -0600

    adding lessons

--- interface.py
+++ interface.py
@@ -7,6 +7,7 @@
 
 
 from PyQt6 import QtCore, QtGui, QtWidgets
+from main import *
 
 
 class Ui_MainWindow(object):
---
+++ teaching.py
@@ -0,0 +1,25 @@
+from collections.abc import ItemsView
+
+from PyQt6.QtGui import QStandardItemModel, QStandardItem
+
+class PythonApp:
+    def __init__(self,main_ui):
+        self.main_ui = main_ui
+        self.current_lesson = 0
+        self.lessons = [
+            "Lesson 1",
+            "Lesson 2",
+            "Lesson 3",
+        ]
+
+        self.tree_view()
+
+    def tree_view(self) -> None:
+
+        model = QStandardItemModel()
+        self.main_ui.treeView.setModel(model)
+        model.appendRow()
+
+    def display_lesson(self, index) ->:
+
+
